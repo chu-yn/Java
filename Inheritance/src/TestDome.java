9d8d78918fc8cf0634f577bd5a52472ca1fc7ea9
ChunYen <panchunyen@gmail.com>
2021-04-08 13:36:01 +0800

    change index of tracks

--- Inheritance/src/TestDome.java
+++ Inheritance/src/TestDome.java
@@ -1,11 +1,11 @@
 public class TestDome {
     public static void main(String[] args) {
         Database database = new Database();
-        CD cd1 = new CD("First try", "ABC", 1, 3);
+        CD cd1 = new CD("First try", "ABC", 0, 3);
         database.addItem(cd1);
         cd1.setComment("Very good");
         cd1.printShortDetails();
-        MP3 mp3 = new MP3("Second try", "DEF", 2, 2);
+        MP3 mp3 = new MP3("Second try", "DEF", 1, 2);
         database.addItem(mp3);
         mp3.setComment("good content");
         mp3.printShortDetails();
