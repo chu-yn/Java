9d07db0b6e805000777041b904cb33809c225370
ChunYen <panchunyen@gmail.com>
2021-05-20 18:06:05 +0800

    clean code

--- TicketMachineGUI/src/Main.java
+++ TicketMachineGUI/src/Main.java
@@ -2,8 +2,6 @@ import source.TicketMachine;
 
 public class Main {
     public static void main(String[] args) {
-        /*TicketMachine ticketMachine = new TicketMachine();
-        ticketMachine.machineProcess(100, 50);*/
         TicketMachine ticketMachine = new TicketMachine();
     }
 }
