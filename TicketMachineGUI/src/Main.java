674858bb5bd0e72f9f9fc6d3bb1238349e523234
ChunYen <panchunyen@gmail.com>
2021-05-20 17:40:34 +0800

    first commit

---
+++ TicketMachineGUI/src/Main.java
@@ -0,0 +1,9 @@
+import source.TicketMachine;
+
+public class Main {
+    public static void main(String[] args) {
+        /*TicketMachine ticketMachine = new TicketMachine();
+        ticketMachine.machineProcess(100, 50);*/
+        TicketMachine ticketMachine = new TicketMachine();
+    }
+}
