3ef5b870e8a76e0c1ea667aa0ba43a5a49a20829
chu-yn <panchunyen@gmail.com>
2021-06-03 11:04:31 +0800

    update version

--- TicketMachineGUI/src/tw/ntou/Main.java
+++ TicketMachineGUI/src/tw/ntou/Main.java
@@ -1,12 +1,11 @@
-/**
- * @version 2.1
- * @author 0086C047 潘俊言
- */
-
 package tw.ntou;
 
 import tw.ntou.source.TicketMachine;
 
+/**
+ * @author 0086C047 潘俊言
+ * @version 2.1
+ */
 public class Main {
     public static void main(String[] args) {
         new TicketMachine();
